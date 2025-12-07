320a9707a56bb7db785f92ace79e940b4370de84
Iurii Mednikov <yurimednikov@proton.me>
2025-12-07 22:08:05 +0100

    Backend: refactored dashboard

--- src/main/java/dev/mednikov/expensetracking/dashboard/dto/IncomeExpenseWidgetDto.java
+++ src/main/java/dev/mednikov/expensetracking/dashboard/dto/IncomeExpenseWidgetDto.java
@@ -1,11 +1,35 @@
 package dev.mednikov.expensetracking.dashboard.dto;
 
+import dev.mednikov.expensetracking.currencies.dto.CurrencyDto;
+
 import java.math.BigDecimal;
 
-public record IncomeExpenseWidgetDto(
-        BigDecimal incomeAmount,
-        BigDecimal expenseAmount,
-        String income,
-        String expense
-) {
+public class IncomeExpenseWidgetDto {
+    private CurrencyDto currency;
+    private BigDecimal income;
+    private BigDecimal expense;
+
+    public CurrencyDto getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(CurrencyDto currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getIncome() {
+        return income;
+    }
+
+    public void setIncome(BigDecimal income) {
+        this.income = income;
+    }
+
+    public BigDecimal getExpense() {
+        return expense;
+    }
+
+    public void setExpense(BigDecimal expense) {
+        this.expense = expense;
+    }
 }
--- src/main/java/dev/mednikov/expensetracking/dashboard/services/DashboardServiceImpl.java
+++ src/main/java/dev/mednikov/expensetracking/dashboard/services/DashboardServiceImpl.java
@@ -1,12 +1,14 @@
 package dev.mednikov.expensetracking.dashboard.services;
 
+import dev.mednikov.expensetracking.currencies.dto.CurrencyDto;
+import dev.mednikov.expensetracking.currencies.dto.CurrencyDtoMapper;
+import dev.mednikov.expensetracking.currencies.models.Currency;
+import dev.mednikov.expensetracking.currencies.repositories.CurrencyRepository;
 import dev.mednikov.expensetracking.dashboard.dto.IncomeExpenseWidgetDto;
 import dev.mednikov.expensetracking.operations.models.Operation;
 import dev.mednikov.expensetracking.operations.models.OperationType;
 import dev.mednikov.expensetracking.operations.repositories.OperationRepository;
 
-import org.joda.money.CurrencyUnit;
-import org.joda.money.Money;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -15,12 +17,14 @@ import java.util.List;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
-    private final static CurrencyUnit defaultCurrency = CurrencyUnit.EUR;
+    private final static CurrencyDtoMapper currencyDtoMapper = new CurrencyDtoMapper();
 
+    private final CurrencyRepository currencyRepository;
     private final OperationRepository operationRepository;
 
-    public DashboardServiceImpl(OperationRepository operationRepository) {
+    public DashboardServiceImpl(OperationRepository operationRepository, CurrencyRepository currencyRepository) {
         this.operationRepository = operationRepository;
+        this.currencyRepository = currencyRepository;
     }
 
     @Override
@@ -31,34 +35,41 @@ public class DashboardServiceImpl implements DashboardService {
 
         // get operations for user and in range
         List<Operation> operations = this.operationRepository.findAllInPeriod(userId, startDate, endDate);
+        Currency currency = this.currencyRepository.findAllByUserId(userId).getFirst();
+        CurrencyDto currencyDto = currencyDtoMapper.apply(currency);
         if (operations.isEmpty()){
             // return empty widget
-            return new IncomeExpenseWidgetDto(
-                    BigDecimal.ZERO,
-                    BigDecimal.ZERO,
-                    Money.zero(defaultCurrency).toString(),
-                    Money.zero(defaultCurrency).toString()
-            );
+            IncomeExpenseWidgetDto incomeExpenseWidgetDto = new IncomeExpenseWidgetDto();
+            incomeExpenseWidgetDto.setCurrency(currencyDto);
+            incomeExpenseWidgetDto.setIncome(BigDecimal.ZERO);
+            incomeExpenseWidgetDto.setExpense(BigDecimal.ZERO);
+            return incomeExpenseWidgetDto;
         }
+        BigDecimal income = operations.stream().filter(o -> o.getType() == OperationType.INCOME)
+                .map(Operation::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal expense = operations.stream().filter(o -> o.getType() == OperationType.EXPENSE)
+                .map(Operation::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+        IncomeExpenseWidgetDto incomeExpenseWidgetDto = new IncomeExpenseWidgetDto();
+        incomeExpenseWidgetDto.setCurrency(currencyDto);
+        incomeExpenseWidgetDto.setIncome(income);
+        incomeExpenseWidgetDto.setExpense(expense);
+        return incomeExpenseWidgetDto;
         // calculate income
-        Money income = operations.stream()
-                .filter(o -> o.getType() == OperationType.INCOME)
-                .map(Operation::getMonetaryAmount)
-                .reduce(Money.zero(defaultCurrency), Money::plus);
-
-        // calculate expense
-        Money expense = operations.stream()
-                .filter(o -> o.getType() == OperationType.EXPENSE)
-                .map(Operation::getMonetaryAmount)
-                .reduce(Money.zero(defaultCurrency), Money::plus);
+//        Money income = operations.stream()
+//                .filter(o -> o.getType() == OperationType.INCOME)
+//                .map(Operation::getMonetaryAmount)
+//                .reduce(Money.zero(defaultCurrency), Money::plus);
+//
+//        // calculate expense
+//        Money expense = operations.stream()
+//                .filter(o -> o.getType() == OperationType.EXPENSE)
+//                .map(Operation::getMonetaryAmount)
+//                .reduce(Money.zero(defaultCurrency), Money::plus);
 
         // create widget dto and return
-        return new IncomeExpenseWidgetDto(
-                income.getAmount(),
-                expense.getAmount(),
-                income.toString(),
-                expense.toString()
-        );
+
     }
 }
